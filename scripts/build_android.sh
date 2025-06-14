f1fb927922cc5ab3fcef39e9e9278e2be2d029fc
mom0ka27 <858479588@qq.com>
2025-06-14 21:57:34 +0800

    feat(action): 并行构建

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -4,4 +4,4 @@ cd app
 
 flutter build apk --release --target-platform android-arm64 --split-per-abi -v
 
-mv app/build/app/outputs/flutter-apk/app-arm64-v8a-release.apk app/build/dist/zero.apk
+mv build/app/outputs/flutter-apk/app-arm64-v8a-release.apk build/dist/zero.apk
