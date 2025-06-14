f2d000bc0350c394119a01657197e2c5d4a53b0b
mom0ka27 <858479588@qq.com>
2025-06-14 21:36:48 +0800

    deps: auto_orientation_v2

--- app/lib/player/controller.dart
+++ app/lib/player/controller.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:auto_orientation/auto_orientation.dart';
+import 'package:auto_orientation_v2/auto_orientation_v2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
