5bc04ad92f7f48c5ddfd62d677a94b4ddf5e778b
mom0ka27 <858479588@qq.com>
2025-06-15 15:04:24 +0800

    fix(action): env

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -2,6 +2,8 @@
 
 cd app
 
-flutter build apk --release --target-platform android-arm64 --split-per-abi -v
+mkdir -p build/dist/
+
+flutter build apk --release --target-platform android-arm64 --split-per-abi
 
 mv build/app/outputs/flutter-apk/app-arm64-v8a-release.apk build/dist/zero.apk
